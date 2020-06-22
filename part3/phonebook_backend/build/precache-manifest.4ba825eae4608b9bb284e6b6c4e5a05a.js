28fd17e779fffbac84ee7254d2e002df4db315fe
theClassicFreak <monsterbanerji@gmail.com>
2020-06-22 02:16:35 -0700

    careful with subdirectory on a sifferent git repo

---
+++ part3/phonebook_backend/build/precache-manifest.4ba825eae4608b9bb284e6b6c4e5a05a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fcba25b0740f8e08ef304c546c1bd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9fb26b35f670164eddb",
+    "url": "/static/css/main.48bf72d6.chunk.css"
+  },
+  {
+    "revision": "8eee79bbe5282a92a8f5",
+    "url": "/static/js/2.02847d1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02847d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9fb26b35f670164eddb",
+    "url": "/static/js/main.1b99427c.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
